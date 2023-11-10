f79a39831dc4f92e0725f080b14661fbd5294dc7
Aron Kvaszta <aronk.221@proton.me>
2023-11-10 18:05:09 +0100

    Y tf cant i debug

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # DONE: When the program runs, open a directory selection dialog
 
-# TODO: Check if selected folder exists
+# DONE: Check if selected folder exists
 
-# TODO: Check if the folder is not considered an important folder by the Windows Operating System
+# DONE: Check if the folder is not considered an important folder by the Windows Operating System
 #           if it's considered an important folder -> 
 #               - tell the user to select a different directory and reopen the dialog
 
@@ -20,6 +20,7 @@
 
 import argparse
 import os
+from shutil import move as mv
 from tkinter import filedialog
 
 
@@ -30,9 +31,10 @@ class Fo_Parser():
             prog="Folder Organizer",
         )
         self.parser.add_argument(
-            "cm",
+            "-cm",
             type=str,
-            help="if 'cm' argument is given, the program will create a new folder called 'MISC', where everything that's not a file will be moved to."
+            help="if '-cm' argument is given, the program will create a new folder called 'MISC', where everything that's not a file will be moved to.",
+            default=None
         )
         self.args = self.parser.parse_args()
     
@@ -41,15 +43,54 @@ class Fo_Parser():
             return False
         else:
             return True
-            
+
+important_folders:list = [
+    "$SysReset",
+    "Program Files",
+    "Program Files (x86)",
+    "ProgramData",
+    "Users",
+    "Windows",
+    "XboxGames",
+]
+
+def valid_directory(dir:str) -> bool:
+    if os.access(dir, os.F_OK) and dir.split('/')[-1] in important_folders:
+        print(f"You probably shouldn't reorganize {dir}.")
+        return False
+    
+    return True
 
 def main():
-    parser = Fo_Parser()
+    parser:Fo_Parser = Fo_Parser()
     
-    directory = filedialog.askdirectory()
+    directory:str = filedialog.askdirectory()
     
-    if os.access(directory, os.F_OK):
-        pass    
+    while not valid_directory(directory):
+        directory = filedialog.askdirectory()
+
+    misc_dir_location:str = ""
+
+    if parser.fo_parse_args() == True:
+        os.mkdir(f"{directory}/MISC")
+        misc_dir_location = f"{directory}/MISC"
+    
+    items_in_directory:list = os.listdir(directory)
+    extensions_in_directory:list = []
+    
+    for item in range(len(items_in_directory)):
+        if os.path.isdir(f"{items_in_directory[item]}") and item != misc_dir_location and misc_dir_location != "":
+            mv(f"{items_in_directory[item]}", misc_dir_location)
+
+    for item in range(len(items_in_directory)):
+        if os.path.isfile(f"{items_in_directory[item]}"):
+            extension = os.path.splitext(items_in_directory[item])[-1]
+            extensions_in_directory.append(extension)
+    
+    print(extensions_in_directory)
+        
+
+        
     
 
 if __name__ == "__main__":
